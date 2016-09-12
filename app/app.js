2daeaef5c28f7ab7505f5df62909eb101e2f38c3
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-09-12 15:07:11 -0500

    routing works

---
+++ app/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var app = angular.module("FlowerApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider.
+  when("/", {
+    controller: ""
+  }).
+  when("/flowers",{
+    templateUrl: "partials/flowers.html",
+    controller: ""
+  }).
+  otherwise("/")
+})
